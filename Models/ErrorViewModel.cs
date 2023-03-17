e7e6d72e161b9970d266b1c2b20416f8ed33d99a
ESTEBAN LÓPEZ RIZZOLO <47069838@ort>
2023-03-17 08:28:26 -0300

    creacion creadora

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_DAI_Brodsky_Rizzolo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
